eecb717f8482a16e88850821c27c95dabf7935fa
Passarelli / Francesco Passarelli <passarellif72@gmail.com>
2023-11-30 17:02:10 +0100

    esercizio completato

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+
+
+let variabileUno = 'Nome';
+document.writeln(variabileUno);
+
+let variabileDue = 'Cognome';
+document.writeln(variabileDue);
+
+let variabileTre = 'Colore preferito';
+document.writeln(variabileTre);
+
+
+
+const x = 55;
+console.log(x);
+let y = 59;
+
+y = x + y;
+
+document.writeln(y);
+
+
+const nome = prompt('Nome:');
+console.log(typeof nome);
+console.log('Valore preso dal prompt:', nome);
+
+console.log(`
+    L'utente ha inserito:
+    ${nome}
+`);
+
+const cognome = prompt('Cognome:');
+console.log(typeof cognome);
+console.log('Valore preso dal prompt:', cognome);
+
+console.log(`
+    L'utente ha inserito:
+    ${cognome}
+`);
+
+const colorePreferito = prompt('Colore preferito:');
+console.log(typeof colorePreferito);
+console.log('Valore preso dal prompt:', colorePreferito);
+
+console.log(`
+    L'utente ha inserito:
+    ${colorePreferito}
+`);
